4ced4a8c720fc3b6745ca2288cf4ee4f54f1b38a
root <jbramall@gmail.com>
2020-03-12 12:05:24 +0000

    fix broken stream

--- stream_to_csv.py
+++ stream_to_csv.py
@@ -3,6 +3,7 @@ import pandas as pd
 import csv
 from twython import Twython
 from twython import TwythonStreamer
+import time
 
 # Load credentials from json file
 with open("twitter_credentials.json", "r") as file:
@@ -20,26 +21,34 @@ def process_tweet(tweet):
 # Filter out unwanted data
 def print_to_term(tweet):
     d = {}
-    d['hashtags'] = [hashtag['text'] for hashtag in tweet['entities']['hashtags']]
     d['text'] = tweet['text']
-    d['user_loc'] = tweet['user']['location']
     return print (d)
 
 # Create a class that inherits TwythonStreamer
-class MyStreamer(TwythonStreamer):     
+class MyStreamer(TwythonStreamer):
 
     # Received data
     def on_success(self, data):
         # Only collect tweets in English
         # if data['lang'] == 'en':
         # tweet_data = process_tweet(data)
-        print_to_term(data)
-        self.save_to_csv(data)
+        try:
+            print_to_term(data)
+        except:
+            pass
+        try:
+            self.save_to_csv(data)
+        except:
+            pass
 
     # Problem with the API
     def on_error(self, status_code, data):
         print(status_code, data)
-        # self.disconnect()
+        self.disconnect()
+        print('stream broken, waiting')
+        time.sleep(5)
+        print('stream broken, restarting')
+        start_stream('corona')
         
     # Save each tweet to csv file
     def save_to_csv(self, tweet):
@@ -47,9 +56,12 @@ class MyStreamer(TwythonStreamer):
             writer = csv.writer(file)
             writer.writerow(list(tweet.values()))
 
+def start_stream(keyword):
+    return stream.statuses.filter(track=keyword)
+
 # Instantiate from our streaming class
 stream = MyStreamer(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'], 
                     creds['ACCESS_TOKEN'], creds['ACCESS_SECRET'])
 # Start the stream
-stream.statuses.filter(track='corona')
-
+# stream.statuses.filter(track='corona')
+start_stream('corona')
